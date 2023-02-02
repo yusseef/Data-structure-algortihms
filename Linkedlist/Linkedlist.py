277ea995ecd7e1bb29c6138a43afd36ef1ab9ba3
Yussef raouf <yussef.raouf11@gmail.com>
2023-02-03 00:01:15 +0200

    linkedlist

--- Linkedlist/Linkedlist.py
+++ Linkedlist/Linkedlist.py
@@ -2,7 +2,6 @@
 A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations.
 The elements in a linked list are linked using pointers
 '''
-
 class Node:
     def __init__(self, data = None, next=None):
         self.data = data
@@ -45,13 +44,44 @@ class LinkedList:
             self.insert_at_the_end(elem)
 
 
+    #Version 2 of linkedlist method
+
+    def length(self):
+        count = 0
+
+        itr = self.head
+        
+        while itr.next:
+            count += 1
+            itr = itr.next
+
+        return f'linkedlist length : {count}'
+
+    def delete(self, data):
+        if self.head is None:
+            return Exception('there is no data left in the list')
+
+        if data == self.head.data:
+            self.head = self.head.next
+
+        itr = self.head
+
+        while itr.next:
+            if data == itr.next.data:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+
+
 
 linked_list = LinkedList()
 linked_list.insert_at_the_beginning(5)
 linked_list.insert_at_the_beginning(7)
 linked_list.insert_at_the_end(9)
 linked_list.insert_values([10, 11, 12])
+linked_list.delete(10)
 
 
 
 linked_list.print()
+print(linked_list.length())
